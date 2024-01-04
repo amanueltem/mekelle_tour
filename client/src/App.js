4387ed16198c988eb456138846a479534e509444
Amanuel Temesgen <amanueltemesgen62@gmail.com>
2024-01-04 07:29:32 +0300

    modfied app.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn How to develop Mekelle tour agency online system.
         </a>
       </header>
     </div>
