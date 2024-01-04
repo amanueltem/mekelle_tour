c75251b3edcaa36bd0a7bbc4fc8a91458c0d9aaa
GetahunH <getahun19h@gmail.com>
2024-01-04 15:13:09 +0300

    i updated map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,7 +1,8 @@
-export default function Map(){
-    return(
-        <div>
-            <h1>Here it continues to see offline Map.</h1>
-        </div>
-    )
+export default function Map() {
+  return (
+    <div>
+      <h1>Here it continues to see offline Map.</h1>
+      <h2>good news !</h2>
+    </div>
+  );
 }
