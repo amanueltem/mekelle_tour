d8d8a2c46f51d118136e3ccbdf5345c864845fba
Amanuel Temesgen <amanueltemesgen62@gmail.com>
2024-01-04 15:16:42 +0300

    I updated map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -3,6 +3,7 @@ export default function Map() {
     <div>
       <h1>Here it continues to see offline Map.</h1>
       <h2>good news !</h2>
+      <h1>THis is interactive map of tigray</h1>
     </div>
   );
 }
