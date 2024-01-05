42949dd7b7d6d1c6d7a8b0699955db081fbe50ad
Amanuel Temesgen <amanueltemesgen62@gmail.com>
2024-01-05 09:30:40 +0300

    actual map comes

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,8 @@
+import Provide from "./Provide";
 export default function Map() {
   return (
     <div>
-      <h1>Here it continues to see offline Map.</h1>
-      <h2>good news !</h2>
-      <h1>THis is interactive map of tigray</h1>
+     <Provide></Provide>
     </div>
   );
 }
