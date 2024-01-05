f55098bab9611e06da0a724d1efdc50e8031963f
Amanuel Temesgen <amanueltemesgen62@gmail.com>
2024-01-05 05:17:17 +0300

    index.js is connected with database

--- server/index.js
+++ server/index.js
@@ -1,31 +1,26 @@
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'ne200ths',
-  database: 'mekelle_tour',
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-  console.log('Connected to the database');
-});
-
-module.exports = connection;
-
-
-const express = require('express');
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
 const app = express();
-const port = 5000;
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "ne200ths",
+  database: "mekelle_tour",
 });
+app.use(express.json());
+app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get("/", (req, res) => {
+  res.json("Hello this is backend!");
+});
+app.get("/map", (req, res) => {
+  const q = "SELECT * FROM places";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    else return res.json(data);
+  });
+});
+app.listen(5000, () => {
+  console.log("Connected to backend at port 5000 okey!");
 });
