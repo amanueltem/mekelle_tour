1a82e89c46860cde3ae1244d397587144f28388b
Amanuel Temesgen <amanueltemesgen62@gmail.com>
2024-01-06 17:06:24 +0300

    i updated map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,8 +1,119 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import React from "react";
 import Provide from "./Provide";
-export default function Map() {
-  return (
-    <div>
-     <Provide></Provide>
+import './style.css';
+
+const  Map = () => {
+  const [places, setPlaces] = useState([]);
+  const [center, setCenter] = useState([13.497402, 39.470737]);
+   const [selectedPlace, setSelectedPlace] = useState(0);
+const[type,setType]=useState(["https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'])
+  useEffect(() => {
+    const fetchAllPlaces = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/map");
+        setPlaces(res.data);
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    // Fetch places only once when the component mounts
+    fetchAllPlaces();
+  }, []); // Empty dependency array means this effect runs once on mount
+
+  useEffect(() => {
+    // Add the non-scrollable style when the component mounts
+    document.body.style.overflow = 'hidden';
+
+    // Clean up the style when the component unmounts
+    return () => {
+      document.body.style.overflow = 'visible';
+    };
+  }, []); 
+
+  const handlePlaceChange = (event) => {
+    const selectedPlaceId = event.target.value; // Use event.target.value
+    setSelectedPlace(selectedPlaceId);
+    
+    // Find the selected place based on place_id
+    const selectedPlaceObj = places.find((place) => place.place_id === parseInt(selectedPlaceId, 10));
+
+    // Set the center based on the selected place
+    setCenter([selectedPlaceObj.latitude, selectedPlaceObj.longitude]);
+  };
+
+const handleType = (event) => {
+  const selectedValue = event.target.value;
+
+  switch (selectedValue) {
+    case "basic":
+      setType([
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      ]);
+      console.log("***************map");
+      break;
+
+    case "satellite":
+      setType([
+        "https://api.maptiler.com/maps/satellite/256/{z}/{x}/{y}.jpg?key=yDEkISZLLKud81ZQCaYT",
+        "https://api.maptiler.com/maps/satellite/256/tiles.json?key=yDEkISZLLKud81ZQCaYT"
+      ]);
+      console.log("************sattelite")
+      break;
+
+    default:
+      // Handle default case if needed
+  }
+};
+
+
+  const SelectPlace = () => (
+    <div className="select-container">
+      <select className="select-element" value={selectedPlace} onChange={handlePlaceChange}>
+       <option value="" disabled>Select a place</option>
+        {places.map((place, index) => (
+          <option key={index} value={place.place_id}>
+            {place.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
-}
+
+ const SelectType = () => (
+  <div className="select-type">
+    <select onChange={handleType}>
+      <option value="" disabled>Select map Type</option>
+      <option key="basic" value="basic">Basic map</option>
+      <option key="satellite" value="satellite">Satellite map</option>
+    </select>
+  </div>
+);
+
+
+  return (
+    <div className="wrapper-container">
+      <table>
+        <tr>
+          <td>
+            <SelectPlace />
+          </td>
+          <td>
+            <SelectType />
+          </td>
+        </tr>
+      </table>
+      {center && (
+        <Provide key={JSON.stringify(center)} className="map-container" center={center}  url={type[0]}
+            attribution={type[1]}/>
+      )}
+    </div>
+  );
+};
+
+export default Map;
+
--- client/src/components/Provide.js
+++ client/src/components/Provide.js
@@ -1,21 +1,55 @@
-import React, { useRef } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-const Provide = () => {
-  const mapRef = useRef(null);
-  const latitude = 13.497402;
-  const longitude = 39.470737;
 
-  return ( 
-    // Make sure you set the height and width of the map container otherwise the map won't show
-      <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/* Additional map layers or components can be added here */}
-      </MapContainer>
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const Provide = (props) => {
+  useEffect(() => {
+    // Update the map center when the 'center' prop changes
+    map?.setView(props.center, map.getZoom());
+  }, [props.center]);
+
+  let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
+  L.Marker.prototype.options.icon = DefaultIcon;
+
+  let map;
+    useEffect(() => {
+    console.log('Props updated:', props.url, props.attribution);
+  }, [props.url, props.attribution]);
+  return (
+    <div>
+      <div style={{ width: '100%', height: '400px', overflowY: 'scroll' }}>
+        <MapContainer
+          center={props.center}
+          zoom={14}
+          style={{ width: '100%', height: '100%' }}
+          whenCreated={(m) => (map = m)} // Save the map instance
+        >
+          <TileLayer
+            url={props.url}
+            attribution={props.attribution}
+          />
+          <Marker position={props.center}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </div>
   );
 };
 
 export default Provide;
+
