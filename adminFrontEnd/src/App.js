4c5ec3e2f2deb44e620cc5b053d4fc0389947788
Amanuel Temesgen <amanueltemesgen62@gmail.com>
2024-01-24 16:37:26 +0300

    architectural shift

---
+++ adminFrontEnd/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import Login from './Login';
+import HomePage from './components/HomePage';
+import AddPackage from './components/package/AddPackage';
+import SeePackages from './components/package/SeePackages';
+import AddPlace from './components/place/AddPlace';
+import Navigation from './components/profile/Navigation';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<div><Navigation /><HomePage /></div>} />
+          <Route path="/add_package" element={<div><Navigation /><AddPackage /></div>} />
+          <Route path="/add_place" element={<div><Navigation /><AddPlace /></div>} />
+          <Route path="/see_packages" element={<div><Navigation /><SeePackages /></div>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
+
---
+++ backend/index.js
@@ -0,0 +1,94 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+import path from 'path'
+import multer from 'multer'
+import getPlaces from "./components/places.js";
+import getAccounts from "./components/Account.js";
+import getAccountsu from "./components/Accountu.js";
+import addPackage from "./components/AddPackage.js";
+import addPlace from "./components/AddPlaceData.js"
+import getPackages from "./components/Packages.js"
+import getPrice from "./components/Prices.js"
+import getPackage from "./components/Package.js"
+
+const app = express();
+const db = mysql.createConnection({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "ne200ths",
+  database: process.env.DB_DATABASE || "mekelle_tour",
+});
+app.locals.db = db;//store db  in locals
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    process.exit(1); // Exit the process if there's a database connection error
+  }
+  console.log("Connected to MySQL!");
+});
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json("Hello this is backend!");
+});
+
+
+
+
+
+
+// Set up Multer to handle file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images'); // Destination folder for uploaded files
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '_' +Date.now()+path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage:storage });
+
+// Handle image upload
+app.post('/upload', upload.single('image'), (req, res) => {
+  //const imageLocation = req.file.path; // This is the path where the image is stored
+  //res.json({ imageLocation });
+  const image=req.file.filename;
+  return res.json(image);
+});
+
+
+
+
+
+
+
+app.get("/map", (req, res) => getPlaces(req, res));
+app.get("/tour_package", (req, res) => getPackages(req, res));
+app.post("/login",(req,res)=>getAccounts(req,res));
+app.post("/loginu",(req,res)=>getAccountsu(req,res))
+app.post("/add_package",(req,res)=>addPackage(req,res));
+app.post("/add_place",(req,res)=>addPlace(req,res));
+app.get("/tour_package", (req, res) => getPackages(req, res));
+app.post("/price",(req,res)=>getPrice(req,res));
+app.post("/package",(req,res)=>getPackage(req,res));
+
+app.use(express.static('public'));
+const host = '0.0.0.0';
+app.listen(8800, host,() => {
+  console.log("Connected to backend at port 8800 okey!");
+});
+
+// Gracefully close the database connection when the server is shutting down
+process.on("SIGINT", () => {
+  console.log("Closing database connection...");
+  db.end((err) => {
+    if (err) {
+      console.error("Error closing MySQL connection:", err);
+    }
+    console.log("MySQL connection closed.");
+    process.exit(0);
+  });
+});
